ffae499c9a444a781cc486811c445c90c934dad3
Alex Storm <alex.ellegaard@yahoo.com>
2019-05-16 20:58:41 +0200

    State - Part 1 - Intro

--- src/App.js
+++ src/App.js
@@ -4,23 +4,6 @@ import './App.css';
 import { Table, Checkbox, Button } from 'semantic-ui-react';
 
 
-const todos = [
-  'Learn React',
-  'Learn Redux',
-  'Learn React Native',
-  'Create a brand new Web App!'
-]
-
-
-const renderTodos = todos => {
-  return todos.map((todo, i) => (
-    <TodoItem key={i}>
-      {todo}
-    </TodoItem>
-
-  ))
-}
-
 
 const TodoItem = ({ children }) => (
   <Table.Row>
@@ -35,29 +18,42 @@ const TodoItem = ({ children }) => (
   </Table.Row>
 )
 
+class App extends Component {
+  state = {
+    todos: [
+      'Learn React',
+      'Learn Redux',
+      'Learn React Native',
+      'Create a brand new Web App!'
+    ]
+  }
 
-function App() {
-  return (
-    <div className="app">
-      <div className="todo-container">
-        <input id="new-todo" className="new-todo" placeholder="What needs to be done?" autoFocus />
-        <label htmlFor="new-todo" style={{ display: 'none' }}>New Todo</label>
-        <Table>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>
-                <Checkbox />
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {renderTodos(todos)}
-          </Table.Body>
-        </Table>
+  render() {
+    return (
+      <div className="app">
+        <div className="todo-container">
+          <input id="new-todo" className="new-todo" placeholder="What needs to be done?" autoFocus />
+          <label htmlFor="new-todo" style={{ display: 'none' }}>New Todo</label>
+          <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>
+                  <Checkbox />
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {this.state.todos.map((todo, i) => (
+                <TodoItem key={i}>{todo}</TodoItem>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
       </div>
-    </div>
 
-  )
+    )
+  }
+
 }
 
 export default App;
