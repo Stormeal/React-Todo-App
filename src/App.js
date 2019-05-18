72010e447f0eaaa360e5dfdc86f9a02640edd85e
Alex Storm <alex.ellegaard@yahoo.com>
2019-05-18 15:59:37 +0200

    Props – Part 2 – (- counter)

--- src/App.js
+++ src/App.js
@@ -6,22 +6,6 @@ import TodoApp from './components/TodoApp'
 
 
 
-
-class Counter extends Component {
-  state = {
-    counter: 0
-  }
-
-  render() {
-    const { counter } = this.state
-    return (
-      <Button onClick={() => {
-        this.setState({ counter: counter + 1 })
-      }}>You have clicked {counter} times!</Button>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
